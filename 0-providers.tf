d5f1c3f32b2729302d43abc02fecee8b46124f97
AkuSeorangManusia <ahsansanadi167@gmail.com>
2026-08-10 17:26:31 +0700

    update .gitignore and configure S3 backend in providers.tf

--- 0-providers.tf
+++ 0-providers.tf
@@ -7,6 +7,14 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "andimsum-tfstate"
+    key            = "terraform.tfstate"
+    region         = "ap-southeast-3"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
